0396b109e39b8b026d93fd074eeecb4c19ea8764
ichwanul09 <129747432+ichwanul09@users.noreply.github.com>
2024-02-17 12:49:54 +0700

    Add files via upload

---
+++ header.php
@@ -0,0 +1,11 @@
+<header class="beach-header">
+    <nav>
+        <ul>
+            <li><a href="#profile">Beranda</a></li>
+            <li><a href="#">Paket Liburan</a></li>
+            <li><a href="#">Aktivitas</a></li>
+            <li><a href="#">Tentang Kami</a></li>
+            <li><a href="#">Kontak</a></li>
+        </ul>
+    </nav>
+</header>
---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PANTAI CANGKRING</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <?php include 'header.php'; ?>
+        <div class="hero-section">
+            <div class="hero-content">
+        <h1>SELAMAT DATANG</h1>
+        <h2>Official PANTAI CANGKING</h2>
+        </div>
+            </div>
+                <div class="profile">
+                <h3>Wisata Pantai Cangkring</h3>
+                <img src="pantai.jpeg" > 
+
+                
+                    
+                <div class="info">
+                <p>Pantai Cangkring merupakan salah satu objek wisata di Bantul, Yogyakarta yang menawarkan pengalaman wisata menarik dengan pemandangan pohon cemara yang indah sebagai latar belakang foto yang sempurna. Lokasinya di daerah Cangkring, Poncosari, Srandakan. Aksesnya menawarkan pemandangan sawah pedesaan yang menawan dan jalur jalan lintas selatan yang menarik. Jika ingin mengunjungi pantai ini cukup membayar parkir dengan tarif 5.000 rupiah untuk motor dan 10.000 rupiah untuk mobil, pantai ini buka dari pukul 08.00 hingga 1800 WIB.</p>
+
+                        <p>Alamat: Alamat Saya</p>
+                    </div>
+                    </div> 
+                
+    </div>
+</body>
+</html>
